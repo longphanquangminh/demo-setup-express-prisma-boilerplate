17880a4c05a889161f31d2e13c356b1de1242e7f
longphanquangminh <lordaizensousuke2001@gmail.com>
2024-05-13 10:56:47 +0700

    push test gh-pages

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from "express";
+import ghpages from "gh-pages";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
+ghpages.publish(
+  "/",
+  {
+    branch: "master",
+    repo: "https://github.com/longphanquangminh/demo-setup-npm-express-prisma-boilerplate.git",
+  },
+  function (err) {
+    console.log(err);
+  },
+);
